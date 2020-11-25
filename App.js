9567002082f95b0ebb529aec2e306eed5f9e8404
Dev-Russo <murilorusso10@gmail.com>
2020-11-25 12:54:43 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  // const [email, setEmail] = useState('');
+  // const [senha, setSenha] = useState('');
+
+  // const logar = () => {
+  //   alert(`${email} ${senha}`);
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+
+      {/* <Text>Email</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        // React - onChange = {event => setEmail(event.target.value)} 
+        onChangeText={text => setEmail(text)}
+        value={email}
+      />
+
+      <Text>Senha</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        // React - onChange = {event => setEmail(event.target.value)} 
+        onChangeText={text => setSenha(text)}
+        value={senha}
+      /> */}
       <StatusBar style="auto" />
     </View>
   );
